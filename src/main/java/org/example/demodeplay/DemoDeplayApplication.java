f7bda55b27449b9cecc7594a9ef41701569f69cc
heartggs <jgggggj6789@gmail.com>
2025-01-16 14:54:42 +0900

    first commit

---
+++ src/main/java/org/example/demodeplay/DemoDeplayApplication.java
@@ -0,0 +1,13 @@
+package org.example.demodeplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDeplayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDeplayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/demodeplay/DemoDeplayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demodeplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDeplayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
