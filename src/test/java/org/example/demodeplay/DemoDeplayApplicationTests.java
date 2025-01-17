c5e41051f536a9a088cc9eff6cd7fabf6f869f5e
heartggs <jgggggj6789@gmail.com>
2025-01-17 11:23:25 +0900

    Feat:gitaction 빌드 작업 추가, 단위테스트 호출 여부, 빌드 결과물 이동 결과

--- src/test/java/org/example/demodeplay/DemoDeplayApplicationTests.java
+++ src/test/java/org/example/demodeplay/DemoDeplayApplicationTests.java
@@ -8,6 +8,10 @@ class DemoDeplayApplicationTests {
 
     @Test
     void contextLoads() {
+        // 단위 테스트
+        // MVC 테스트 -> 목업을 통해서 가상 처리 가능
+        // RestAPI 테스트 가능
+        System.out.println("단위 테스트 호출!! echo");
     }
 
 }
